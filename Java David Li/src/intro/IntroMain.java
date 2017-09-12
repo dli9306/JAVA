7b3a9f732c121be068a4ff8c9219adb1819c47f3
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2017-09-12 11:45:29 -0400

    STUFF

---
+++ Java David Li/src/intro/IntroMain.java
@@ -0,0 +1,12 @@
+package intro;
+
+public class IntroMain {
+	public static void main(String[] args) {
+		//declaration of a local variable
+		// notice the use of the word 'new'
+		// new must be used to call a constructor
+		CodingCoventions conventionsInstance = new CodingCoventions();
+	}
+}}
+
+}
