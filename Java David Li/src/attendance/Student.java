a0b1c43edb75d65c0caa361756c51ede689a9a96
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2017-09-26 11:46:46 -0400

    t4t4

--- Java David Li/src/attendance/Student.java
+++ Java David Li/src/attendance/Student.java
@@ -1,10 +1,55 @@
 package attendance;
 
-public class Student {
+public class Student implements Attendee {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private boolean present;
+	public Student(firstName,lastName){}
+	@Override
+	public boolean isPresent() {
+		if(present)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
+
+	@Override
+	public void setPresent(boolean present) {
+		this.present = present;
+		
+	}
+
+	@Override
+	public String getFirstName() {
+		return "David";
+	}
+
+	@Override
+	public String getLastName() {
+		return "Li";
+	}
+
+	@Override
+	public boolean mathces(String first, String last) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean matches(String last) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getReportString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
 
 }
