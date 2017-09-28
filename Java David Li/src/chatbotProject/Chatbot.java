6d5d42e91387b0a114f7d6960c0e06cd23c6c307
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2017-09-28 11:45:58 -0400

    fds

--- Java David Li/src/chatbotProject/Chatbot.java
+++ Java David Li/src/chatbotProject/Chatbot.java
@@ -1,5 +1,34 @@
 package chatbotProject;
 
 public class Chatbot {
+	
+	private String userName;
+	private Topic david;
+	private boolean chatting;
+	
+	public Chatbot() {
+		david = new ChatbotDavid();
+		userName = "unknown user";
+		chatting = true;
+	}
+
+	public void startTalking() {
+		ChatbotMain.print("Welcome to our chatbot! what is your name?");
+		userName = ChatbotMain.getInput();
+	    while(chatting){
+	    	ChatbotMain.print("What do you want to talk about");
+	    	String response = ChatbotMain.getInput();
+	    	if(david.isTriggered(response)) {
+	    		chatting = false;
+	    		david.startChatting();
+	    	}
+	    	else
+	    	{
+	    		ChatbotMain.print("I'm sorry, I dont understand");
+	    	}
+	    	
+	    }
+		
+	}
 
 }
