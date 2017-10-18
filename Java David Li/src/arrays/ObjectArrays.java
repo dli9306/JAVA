1a2a5815139e51ab4a555930735219b74e3c253f
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2017-10-18 11:14:23 -0400

    x

--- Java David Li/src/arrays/ObjectArrays.java
+++ Java David Li/src/arrays/ObjectArrays.java
@@ -12,8 +12,25 @@ public class ObjectArrays {
 	}
 
 	private void populate(Person[] people) {
-		// TODO Auto-generated method stub
-		
+		for(int i=0;i < people.length;i++) {
+    		String firstName = randomNameFrom(Person.FIRST_START,Person.FIRST_MIDDLE,Person.FIRST_END);
+    		String lastName = randomNameFrom(Person.LAST_START,Person.LAST_MIDDLE,Person.LAST_END);
+    		Borough b = randomBorough();
+    		people[i] = new Person(firstName,lastName,b);
+		}
+
+}
+
+	private Borough randomBorough() {
+		return Borough.NY_BOROUGHS[(int)(Math.random()*Borough.NY_BOROUGHS.length)];
 	}
 
+	private String randomNameFrom(String[] a, String[] b, String[] c) {
+		
+		return get(a)+get(b)+get(c);
+	}
+
+	private String get(String[] a) {
+		return a[(int)(Math.random()*a.length)];
+	}
 }
