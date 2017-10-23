7864a1a0406d888a82a4d526859692a3cf67d820
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2017-10-23 11:14:24 -0400

    x

---
+++ Java David Li/src/caveExplorer/CaveRoom.java
@@ -0,0 +1,45 @@
+package caveExplorer;
+
+public class CaveRoom {
+	
+	private String description;//tells what the room looks like
+	private String directions;//tells what you can do
+	private String contents;//a symbol representing what's in the room
+	private String defaultContents;
+	//the rooms are organized by direction, 'null' signifies no room/door in that direction
+	private CaveRoom[] borderingRooms;
+	private Door[] doors;
+	
+	//constants
+	public static final int NORTH =0;
+	public static final int EAST = 1;
+	public static final int SOUTH = 2;
+	public static final int WEST = 3;
+
+	public CaveRoom() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	public void setDefaultContents(String defaultContents) {
+		this.defaultContents = defaultContents;
+	}
+	
+	
+
+}
