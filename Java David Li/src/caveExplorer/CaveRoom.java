b9126ea43fc7ec8b5ff9808e9fa00c8196934aae
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2017-10-24 11:39:46 -0400

    xx

--- Java David Li/src/caveExplorer/CaveRoom.java
+++ Java David Li/src/caveExplorer/CaveRoom.java
@@ -67,6 +67,76 @@ public class CaveRoom {
 		String[] directions = {"the North"," the East"," the South"," the West"};
 		return directions[dir];
 	}
+	
+	public void enter() {
+		contents = "X";
+	}
+	
+	public void leave() {
+		contents = defaultContents;
+	}
+	
+	/**
+	 * Gives this room access to anotherRoom (and vice-versa)
+	 * and sets a door between them, updating the directions
+	 * @param direction
+	 * @param anotherRoom
+	 * @param door
+	 */
+	
+	public void setConnection(int direction, CaveRoom anotherRoom, Door door) {
+		addRoom(direction,anotherRoom,door);
+		anotherRoom.addRoom(oppositeDirection(direction), this, door);
+	  }
+
+	public static int oppositeDirection(int direction) {
+		return (direction+2)%4;
+	}
+
+	public void addRoom(int direction, CaveRoom cave, Door door) {
+		borderingRooms[direction] = cave;
+		doors[direction]=door;
+		setDirections();
+		
+	}
+	
+	public void interpretInput(String input) {
+		while(isValid(input)) {
+			System.out.println("You can only enter 'w','a','s', or'd'");
+			input = CaveExplorer.in.nextLine();
+		}
+		//task: convert user input into a direction
+		//DO NOT USE AN IF STATEMENT
+		//or if you must, don't use more than one
+		String dirs ="wdsa";
+		goToRoom(dirs.indexOf(input));
+	}
+
+	private boolean isValid(String input) {
+		String validEntries = "wdsa";
+		return validEntries.indexOf(input) > -1 && input.length() ==1;
+	}
+
+	private void goToRoom(int direction) {
+	   //first, protect against null pointer exception
+	   // user cannot go through a non-existent door
+		if(borderingRooms[direction] != null && doors[direction] != null) {
+			CaveExplorer.currentRoom.leave();
+			CaveExplorer.currentRoom = borderingRooms[direction];
+			CaveExplorer.currentRoom.enter();
+			CaveExplorer.inventory.updateMap();
+			
+		}
+		
+	}
+	
+	/**
+	 * This will be where you group sets up all the caves
+	 * and all the connections
+	 */
+	public static void setUpCaves() {
+		
+	}
 
 	public String getDescription() {
 		return description;
