e7f33e849cc6ce0aba574683bb0bcda2e36ef705
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2017-11-01 11:45:12 -0400

    xx

--- Java David Li/src/caveExplorer/CaveExplorer.java
+++ Java David Li/src/caveExplorer/CaveExplorer.java
@@ -9,6 +9,7 @@ public class CaveExplorer {
 	public static CaveRoom currentRoom; //changes based on how the user navigated
 	public static Inventory inventory;//where all objects found in cave are kept
 	public static boolean playing = true;
+	public static NPC[] npcs;
 	
 	public static void main(String[] args) {
 		in = new Scanner(System.in);
@@ -21,6 +22,7 @@ public class CaveExplorer {
 
 	private static void startExploring() {
 		while(playing) {
+			npcActions();
 			print(inventory.getDescription());
 			print(currentRoom.getDescription());
 			print("What would you like to do?");
@@ -29,6 +31,14 @@ public class CaveExplorer {
 		}
 	}
 	
+	private static void npcActions() {
+		for(NPC n: npcs) {
+			n.act();
+		}
+		inventory.updateMap();
+		
+	}
+
 	public static void print(String s) {
 		//NOTE: later you can replace this line with the more sophisticated "multiLinePrint" from Chatbot
 		System.out.println(s);
