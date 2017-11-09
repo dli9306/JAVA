df37ecc55a0d8adf07da45353fb45d7fadb28e3f
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2017-11-09 10:34:16 -0500

    zxx

---
+++ Java David Li/src/javaTheHardWay/Environment.java
@@ -0,0 +1,37 @@
+package javaTheHardWay;
+
+public class Environment {
+	
+    private int plants;
+    private int growthRate;
+    
+	public Environment(int plants) {
+		this.plants = plants;
+		this.growthRate = plants/2;
+	}
+
+	public static void main(String[] args) {
+		Environment farm = new Environment(10);
+		System.out.println(farm);
+		farm.growPlants();
+
+	}
+
+	private void growPlants() {
+		plants+= growthRate;
+		System.out.println("\n There are "+plants +" plants");
+		
+	}
+	
+	public boolean eatPlants() {
+		if(plants >0) {
+			plants--;
+			return true;
+		}
+		return false;
+	}
+	public String toString() {
+		return "An vast field";
+	}
+
+}
