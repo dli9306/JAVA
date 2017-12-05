3fa239edc4a01331f3c158e8ca327816091d8c01
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2017-12-05 10:41:03 -0500

    x

---
+++ Java David Li/src/Recursion/Fibonacci.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		int x = fibonacciSeq(4);
+		System.out.println(x);
+
+	}
+	public static int fibonacciSeq(int n) {
+		if(n==1 || n==0) {
+			return 1;
+		}
+		else {
+			return 
+		}
+	}
+
+}
